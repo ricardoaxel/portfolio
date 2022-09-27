e8021a4ae8e599fb25353a3961ebaa3d88669321
Axel <ricardoaxelgd@gmail.com>
2022-09-27 17:55:35 -0400

    configuration almost completed

---
+++ .prettierrc.js
@@ -0,0 +1,9 @@
+module.exports = {
+    semi: false,
+    trailingComma: 'es5',
+    singleQuote: false,
+    jsxSingleQuote:  false,
+    printWidth: 80,
+    tabWidth: 2,
+    endOfLine: 'auto'
+}
--- src/App.tsx
+++ src/App.tsx
@@ -1,3 +1,17 @@
+import "./styles.css"
+import image from "./assets/react.jpg"
+import svgex from "./assets/exampleSVG.svg"
+import { ClikCounter } from "./assets/ClikCounter"
 export const App = () => {
-  return <h1>React</h1>;
-};
+  return (
+    <>
+      <h1>
+        Hello Reacti - {process.env.NODE_ENV} {process.env.name}
+      </h1>
+      <ClikCounter />
+      <img src={image} alt={"asdad"} />
+      dsfsfs
+      <img src={svgex} alt={"asdad"} />
+    </>
+  )
+}
