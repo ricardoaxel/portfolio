155ffef5e88961e0b16291e5daa66d04e6b66077
Axel <ricardoaxelgd@gmail.com>
2022-09-27 18:01:17 -0400

    test commit of eslint

--- .eslintrc.js
+++ .eslintrc.js
@@ -21,6 +21,7 @@ module.exports = {
     "prettier/@typescript-eslint",
     "plugin:prettier/recommended",
   ],
+
   rules: {
     "no-unused-vars": "off",
     "@typescript-eslint/no-unused-vars": ["error"],
