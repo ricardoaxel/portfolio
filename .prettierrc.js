13d3a3fd4fa3848de14ad1f7212787e2378877a5
Axel <ricardoaxelgd@gmail.com>
2022-09-27 17:59:52 -0400

    double quotes fixed

--- .prettierrc.js
+++ .prettierrc.js
@@ -1,9 +1,9 @@
 module.exports = {
-    semi: false,
-    trailingComma: 'es5',
-    singleQuote: false,
-    jsxSingleQuote:  false,
-    printWidth: 80,
-    tabWidth: 2,
-    endOfLine: 'auto'
+  semi: false,
+  trailingComma: 'es5',
+  singleQuote: false,
+  jsxSingleQuote: false,
+  printWidth: 80,
+  tabWidth: 2,
+  endOfLine: 'auto',
 }
