e23abb8e9416c99f8f02521c44704b4de2512477
Axel <ricardoaxelgd@gmail.com>
2022-09-27 22:48:41 -0400

    Emotion added

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,21 @@
-import "./styles.css"
+// import "./styles.css"
 import image from "./assets/react.jpg"
 import svgex from "./assets/exampleSVG.svg"
 import { ClikCounter } from "./assets/ClikCounter"
+import { css } from "@emotion/react"
+
+const cardCss = {
+  self: css({
+    backgroundColor: "#cf2a2a",
+    border: "1px solid #eee",
+    borderRadius: "0.5rem",
+    padding: "1rem",
+  }),
+}
+
 export const App = () => {
   return (
-    <>
+    <div css={cardCss.self}>
       <h1>
         Hellssdfso Reacti - {process.env.NODE_ENV} {process.env.name}
       </h1>
@@ -12,6 +23,6 @@ export const App = () => {
       <img src={image} alt={"asdad"} />
       dsfsfs
       <img src={svgex} alt={"asdad"} />
-    </>
+    </div>
   )
 }
